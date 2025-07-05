e44421f63df22fe3725eeb1daea74e54269b2d3e
Amirreza Ansari <amirrezaansari@5452>
2025-07-06 01:12:43 +0330

    add smooth scrolling

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Making the mobile navigation work
 const btnMobileNav = document.querySelector('.btn-mobile-nav');
 
-const header = document.querySelector('.header-section');
+const headerEl = document.querySelector('.header-section');
 
 btnMobileNav.addEventListener('click', function () {
-  header.classList.toggle('open-nav');
+  headerEl.classList.toggle('open-nav');
 });
 
 // updating year
@@ -12,3 +12,31 @@ const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 
 yearEl.textContent = currentYear;
+
+// Smooth Scrolling
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('open-nav');
+    }
+  });
+});
