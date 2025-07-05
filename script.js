a317e0c843d629fc2d3be4d6062ecbf32dbc945a
Amirreza Ansari <amirrezaansari@5452>
2025-07-06 00:06:36 +0330

    update index.html and the year span

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Making the mobile navigation work
 const btnMobileNav = document.querySelector('.btn-mobile-nav');
 
 const header = document.querySelector('.header-section');
@@ -5,3 +6,9 @@ const header = document.querySelector('.header-section');
 btnMobileNav.addEventListener('click', function () {
   header.classList.toggle('open-nav');
 });
+
+// updating year
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
